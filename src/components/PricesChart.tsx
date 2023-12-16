e7c93ab1763922e31761e8dc0fcf8cb004cb8b90
Ayush Abraham <ayushabraham01@gmail.com>
2023-12-16 20:47:19 +1100

    somewhat fixed dropdown. Need to choose different chart library

--- src/components/PricesChart.tsx
+++ src/components/PricesChart.tsx
@@ -2,7 +2,9 @@ import { Text, View } from "react-native";
 import Price from "../model/Price";
 import { LineChart } from "react-native-chart-kit";
 import { Dimensions } from "react-native";
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Dataset } from "react-native-chart-kit/dist/HelperTypes";
+import { LineChartData } from "react-native-chart-kit/dist/line-chart/LineChart";
 
 
 
@@ -10,95 +12,186 @@ function PricesChart(props: { prices: Price[] }) {
 
     const { prices } = props
 
-    const [data, setData] = useState({
-        labels: ['a'],
-        datasets: [
-            {
-                data: [0]
-            }
-        ],
-        legend: ['a']
-    });
+    const [data, setData] = useState<LineChartData>(
+        // {
+        //     labels: ['a'],
+        //     datasets: [
+        //         {
+        //             data: [1, null, 3]
+        //         },
+        //         {
+        //             data: [4, 5, 6]
+        //         },
+
+        //     ],
+        //     legend: ['a', 'b']
+        // }
+        {
+            "datasets": [
+                {
+                    "data": [14, 15, 16]
+                },
+                {
+                    "data": [13, 11, 19, 14]
+                }
+            ],
+            "labels": ["01-12", "02-12", "03-12", "05-12", "07-12"],
+            "legend": ["Store 1", "Store 2"],
+        }
+    );
+
     const [isLoaded, setIsLoaded] = useState(false);
 
     const screenWidth = Dimensions.get("window").width;
     const chartConfig = {
-        backgroundGradientFrom: "#1E2923",
-        backgroundGradientFromOpacity: 0,
-        backgroundGradientTo: "#08130D",
-        backgroundGradientToOpacity: 0.5,
-        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-        strokeWidth: 2, // optional, default 3
-        barPercentage: 0.5,
-        useShadowColorFromDataset: false // optional
+        backgroundColor: '#1cc910',
+        backgroundGradientFrom: '#eff3ff',
+        backgroundGradientTo: '#efefef',
+        decimalPlaces: 2,
+        color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+        style: {
+            borderRadius: 16,
+        },
     };
 
 
 
-    // useEffect(() => {
-        useLayoutEffect(()=> {
+    useEffect(() => {
 
-        const sortedPrices = prices.sort((a, b) => {
-            if (Date.parse(a.noted_at) < Date.parse(b.noted_at)) {
-                return -1;
-            }
-            if (Date.parse(a.noted_at) > Date.parse(b.noted_at)) {
-                return 1;
-            }
-            return 0;
-        });
+        if (prices.length != 0) {
+            const sortedPrices = prices.sort((a, b) => {
+                if (Date.parse(a.noted_at) < Date.parse(b.noted_at)) {
+                    return -1;
+                }
+                if (Date.parse(a.noted_at) > Date.parse(b.noted_at)) {
+                    return 1;
+                }
+                return 0;
+            });
 
-        const allDates: string[] = sortedPrices.map(price => price.noted_at)
-        // const allDates: string[] = [...new Set(prices.map(price => price.noted_at))]
 
-        const costsByStore: { [key: string]: number[] } = sortedPrices.reduce(
-            (result, obj) => {
-                const key: string = obj.store_id;
+            // Step 1: Group data by date and item_id
+            const groupedData: { [key: string]: Price[] } = sortedPrices.reduce((acc, obj) => {
+                const key = `${obj.noted_at}-${obj.store_id}`;
+                if (!acc[key]) {
+                    acc[key] = [];
+                }
+                acc[key].push(obj);
+                return acc;
+            }, {} as { [key: string]: Price[] });
 
-                // Check if the key already exists in the result object
-                if (!result[key]) {
-                    // If the key doesn't exist, create a new array for that key
-                    result[key] = [];
+
+
+            // Step 2 and 3: Interpolate missing values
+            const interpolatedData = Object.values(groupedData).flatMap((group) => {
+                group.sort((a, b) => Date.parse(a.noted_at) - Date.parse(b.noted_at));
+                const result = [];
+
+                for (let i = 0; i < group.length; i++) {
+                    result.push(group[i]);
+
+                    if (i < group.length - 1) {
+                        const currentDate = new Date(group[i].noted_at);
+                        const nextDate = new Date(group[i + 1].noted_at);
+
+                        let Difference_In_Time = nextDate.getTime() - currentDate.getTime();
+
+                        // To calculate the no. of days between two dates
+                        const daysDifference =
+                            Math.round(Difference_In_Time / (1000 * 3600 * 24));
+
+                        // const daysDifference = (nextDate - currentDate) / (1000 * 60 * 60 * 24);
+
+                        if (daysDifference > 1) {
+                            // Interpolate missing values
+                            const interpolatedCost =
+                                (group[i + 1].cost + group[i].cost) / 2; // Average cost
+                            for (let j = 1; j < daysDifference; j++) {
+                                const interpolatedDate = new Date(
+                                    currentDate.getTime() + j * 24 * 60 * 60 * 1000
+                                ).toISOString().split('T')[0];
+                                result.push({
+                                    noted_at: interpolatedDate,
+                                    cost: interpolatedCost,
+                                    store_id: group[i].store_id,
+                                });
+                            }
+                        }
+                    }
                 }
 
-                // Push the current object to the array associated with the key
-                result[key].push(obj.cost);
-
                 return result;
-            },
-            {} as { [key: string]: number[] }
-        );
+            });
 
-        console.log(costsByStore);
-
-        console.log(allDates)
+            console.log('interpolated ', interpolatedData);
 
 
-        const datasets = []
-        for (const [key, value] of Object.entries(costsByStore)) {
-            datasets.push({ data: value })
+
+
+
+
+
+
+            const allDates: string[] = interpolatedData.map(price => price.noted_at)
+
+
+            const costsByStore: { [key: string]: number[] } = interpolatedData.reduce(
+                (result, obj) => {
+                    const key: string = obj.store_id;
+
+                    if (!result[key]) {
+                        result[key] = [];
+                    }
+
+                    result[key].push(obj.cost);
+
+                    return result;
+                },
+                {} as { [key: string]: number[] }
+            );
+
+            console.log('costsbystore', costsByStore);
+
+            console.log('alldates: ', allDates)
+
+
+            const priceDatasets: Dataset[] = []
+            for (const [key, value] of Object.entries(costsByStore)) {
+                const singlePriceDataset: Dataset = {
+                    data: value,
+                    color: (opacity) => `rgb(0, 0, ${opacity * 255})`,
+                }
+                priceDatasets.push(singlePriceDataset)
+            }
+
+            console.log('pricedatasets', priceDatasets)
+
+            const priceLegend = [...new Set(sortedPrices.map(price => price.store_id))]
+
+            const newData: LineChartData = {
+                labels: allDates,
+                datasets: priceDatasets,
+                legend: priceLegend
+            }
+
+            // setData(newData)
+
+            console.log('newdata', newData)
+
+            console.log('data after setting', data)
+            console.log(data.datasets)
+
+            console.log('data is set')
+
+            setIsLoaded(true)
         }
 
-        console.log(datasets)
 
-        const legend = sortedPrices.map(price => price.store_id)
-
-
-
-        setData({
-            labels: allDates,
-            datasets: datasets,
-            legend: legend
-        })
-
-        setIsLoaded(true)
-
-
-    }, [])
+    }, [prices])
 
 
 
-    console.log(data)
+
 
     return (
         <View>
